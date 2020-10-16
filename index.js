baad8c7073ae60a204cd37df4128729cbd5c9946
lordeesu <72955678+lordeesu@users.noreply.github.com>
2020-10-16 17:22:20 -0300

    nice

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN);
+const prefix = "pb ";
+client.on('ready', () => {
+	console.log('Bot running.');
+});
+client.on("message", function(message) {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	const commandBody = message.content.slice(preifx.length);
+	const args = commandBody.split (' ');
+	const command = args.shift().toLowerCase();
+	if (command === "ping") {
+		const timeTaken = date.now() - message.createdTimestamp;
+		message.reply ('<:ping_pong:766746745173704705>Pong! Latencia de ${timeTaken]ms.');
+});
+
+
+
+
